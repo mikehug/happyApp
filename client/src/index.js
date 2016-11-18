dad4c3aeb8aa69183f9c3d444d1b8145eebc533f
Mike Hughes <mikehug@gmail.com>
2016-11-18 14:35:25 +0000

    bootstrap css paper file and font move to src

--- client/src/index.js
+++ client/src/index.js
@@ -6,19 +6,19 @@ import SignUp from './SignUp';
 import SignIn from './SignIn';
 import Dashboard from './Dashboard';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-paper.css';
+import './bootstrap-paper.css';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      {/* add childern of app here*/}
-      <IndexRoute component={Dashboard}/>
-      <Route path="/chat" component={Chat}/>
-      <Route path="/signup" component={SignUp}/>
-      <Route path="/signin" component={SignIn}/>
-    </Route>
-  </Router>
-  , document.getElementById('root')
+	<Router history={browserHistory}>
+		<Route path="/" component={App}>
+			{/* add childern of app here*/}
+			<IndexRoute component={Dashboard}/>
+			<Route path="/chat" component={Chat}/>
+			<Route path="/signup" component={SignUp}/>
+			<Route path="/signin" component={SignIn}/>
+		</Route>
+	</Router>
+	, document.getElementById('root')
 );
