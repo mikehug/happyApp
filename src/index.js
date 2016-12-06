12998065b038e9a0d955576de8d9744b332e67f4
Mike Hughes <mikehug@gmail.com>
2016-12-06 05:20:24 +0000

    log public folder in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 'use strict';
-
+const path = require('path');
 const app = require('./app');
 const port = app.get('port');
 const server = app.listen(port);
 
+console.log(path.parse(__dirname));
 server.on('listening', () =>
 	console.log(`Feathers application started on ${app.get('host')}:${port} and public folder of ${app.get('public')}` )
 );
